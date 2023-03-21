3778560477db7e0dae7947d8e2745436f35a5a6b
Borys Kharchenko <arximus88@gmail.com>
2023-03-22 00:49:33 +0200

    pages added, new components

---
+++ src/routes/cats/+page.svelte
@@ -0,0 +1,13 @@
+<!-- src/routes/About.svelte -->
+<script>
+    // Add your component logic here, if needed
+  </script>
+  
+  <style>
+    /* Add your component-specific styles here, if needed */
+  </style>
+  
+  <main>
+    <h1>Cats</h1>
+    <p>Welcome to the Projects page!</p>
+  </main>
