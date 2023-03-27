1445b1be1ae7752a5b20123f4ecf458a03102195
Borys Kharchenko <arximus88@gmail.com>
2023-03-27 16:17:07 +0300

    quotes

---
+++ src/lib/components/PersonalAvatar.svelte
@@ -0,0 +1,28 @@
+<script>
+    export let bg = '#FFAC8A';
+    export let number = Math.floor(Math.random() * 12) + 1;
+  
+    const style = `background-color: ${bg}`;
+  </script>
+  
+  <div class="avatar" style={style}>
+    <img src={`images/avatar-${number}.png`} alt="avatar" />
+  </div>
+  
+  <style>
+    .avatar {
+      width: 100px;
+      height: 100px;
+      border-radius: 50%;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    }
+  
+    img {
+      width: 80%;
+      height: 80%;
+      border-radius: 50%;
+    }
+  </style>
+  
---
+++ src/lib/components/PersonalQuote.svelte
@@ -0,0 +1,50 @@
+<script>
+    import PersonalAvatar from './PersonalAvatar.svelte';
+    export let bg = '#FFAC8A';
+    export let number = 1;
+    export let width = 'fullwide';
+    export let text = 'Enter your quote here';
+  </script>
+  
+  <div class="personal-quote" style="background-color: {bg}; width: {width};">
+    <PersonalAvatar bg={bg} number={number} />
+  
+    <div class="quote-text">
+      <p>{text}</p>
+    </div>
+  </div>
+  
+  <style>
+    .personal-quote {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      border-radius: 8px;
+      padding: 16px;
+      box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+    }
+  
+    .personal-quote .quote-text {
+      background-color: #ffffff;
+      border-radius: 8px;
+      padding: 16px;
+      flex-grow: 1;
+    }
+  
+    :global(.personal-quote .quote-text p) {
+      margin: 0;
+    }
+  
+    :global(.personal-quote.fullwide) {
+      width: 100%;
+    }
+  
+    :global(.personal-quote.long) {
+      width: 512px;
+    }
+  
+    :global(.personal-quote.small) {
+      width: 320px;
+    }
+  </style>
+  
