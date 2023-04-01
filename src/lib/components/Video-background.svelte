aafe3a85c9f53a6fd6dd4050919476d76e3d3ef3
Borys Kharchenko <arximus88@gmail.com>
2023-04-01 23:25:37 +0300

    evening update

--- src/lib/components/Video-background.svelte
+++ src/lib/components/Video-background.svelte
@@ -1,40 +1,43 @@
 <div class="bg-container">
-    <video class="bg-video" autoplay muted loop poster="/images/bg-img.webp">
-        <source src="/media/bg-vid-dark_1.webm" type="video/webm">
-        <source src="/media/bg-vid-dark_1.mp4" type="video/mp4">
-        Your browser does not support the video tag.
-</video>
+	<video class="bg-video" autoplay muted loop poster="/images/bg-img.webp">
+		<source src="/media/bg-vid-dark_1.webm" type="video/webm" />
+		<source src="/media/bg-vid-dark_1.mp4" type="video/mp4" />
+		<track src="/media/captions_en.vtt" kind="captions" srclang="en" label="english_captions" />
+		Your browser does not support the video tag.
+	</video>
 </div>
 
 <style>
-    /* Background */
-    .bg-container {
-        position: absolute;
-        right: 0;
-        top: 0;
-        bottom: 0;
-        z-index: -1;
-    }
+	/* Background */
+	.bg-container {
+		position: absolute;
+		right: 0;
+		top: 0;
+		bottom: 0;
+		z-index: -1;
+	}
 
-    .bg-video {
-        height: 64vh;
-    }
+	.bg-video {
+		height: 64vh;
+	}
 
-    .bg-container::before {
-        content: "";
-        position: absolute;
-        width: 100%;
-        height: 64vh;
+	.bg-container::before {
+		content: '';
+		position: absolute;
+		width: 100%;
+		height: 64vh;
 
-        z-index: 1;
-        background:  radial-gradient(50% 100% at 50% 0%, rgba(25, 27, 29, 0.32) 0%, #191B1D 100%);
-    }
+		z-index: 1;
+		background: radial-gradient(50% 100% at 50% 0%, rgba(25, 27, 29, 0.32) 0%, #191b1d 100%);
+	}
 
-    :global([data-theme="light"]) .bg-video {filter: invert(100%) hue-rotate(140deg);}
+	:global([data-theme='light']) .bg-video {
+		filter: invert(100%) hue-rotate(140deg);
+	}
 
-    @media (prefers-reduced-motion: reduce) {
-    .bg-video {
-        display: none;
-    }
-    }
+	@media (prefers-reduced-motion: reduce) {
+		.bg-video {
+			display: none;
+		}
+	}
 </style>
