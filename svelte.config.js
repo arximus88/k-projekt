a22f594f2b94041af39f9308a8eacf1b2fac4aeb
Borys Kharchenko <arximus88@gmail.com>
2023-04-15 23:28:34 +0300

    for build

--- svelte.config.js
+++ svelte.config.js
@@ -3,14 +3,16 @@ import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		adapter: adapter()
-	},
-	preprocess: preprocess({
-		typescript: {
-			tsconfigFile: './tsconfig.json'
-		}
-	})
+  kit: {
+    adapter: adapter(),
+  },
+  preprocess: [
+    preprocess({
+      scss: {
+        includePaths: ['src/styles'],
+      },
+    }),
+  ],
 };
 
 export default config;
